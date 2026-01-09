385f99c9d41a9aa4e2033f0ae94c1f92da7a41a8
Taber Miyauchi <tabermiyauchi@gmail.com>
2026-01-09 12:46:40 -1000

    Initial commit: shared utilities library for cross-repo demo

---
+++ shared_utils_simple/__init__.py
@@ -0,0 +1,5 @@
+"""Shared utilities library."""
+
+from .math import add
+
+__all__ = ["add"]
---
+++ shared_utils_simple/math.py
@@ -0,0 +1,6 @@
+"""Simple math utilities."""
+
+
+def add(a: float, b: float) -> float:
+    """Add two numbers."""
+    return a + b
